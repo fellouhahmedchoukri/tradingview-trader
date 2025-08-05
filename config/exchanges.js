87b1f88645c497be9290c6c13dc0029cbd1eedcd
FAC <57494827+fellouhahmedchoukri@users.noreply.github.com>
2025-08-05 20:39:41 +0100

    Update exchanges.js

--- config/exchanges.js
+++ config/exchanges.js
@@ -1,13 +1,16 @@
 export default {
   selectedExchange: 'binance',
-  accountBalance: 1000,
+  accountBalance: 1000, // Mise à jour automatique recommandée
   
   exchanges: {
-    bybit: {
+    binance: {
       apiKey: process.env.BINANCE_API_KEY,
       secret: process.env.BINANCE_API_SECRET,
       futures: true,
-      options: { adjustForTimeDifference: true }
+      testnet: process.env.TESTNET === 'true' || false,
+      options: {
+        defaultType: 'future'
+      }
     }
   }
 };
