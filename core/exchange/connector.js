fec7af5ac1c2dc18c7a910b88888a4e406cbb903
FAC <57494827+fellouhahmedchoukri@users.noreply.github.com>
2025-08-05 20:20:28 +0100

    Update connector.js

--- core/exchange/connector.js
+++ core/exchange/connector.js
@@ -10,6 +10,6 @@ export const connect = (exchangeId, config) => {
     options: { adjustForTimeDifference: true }
   });
   
-  exchange.setSandboxMode(false); // Mode production
+  exchange.setSandboxMode(false);
   return exchange;
 };
