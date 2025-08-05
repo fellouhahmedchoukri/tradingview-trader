2a25ffccc04f81a4d3ed86b3ba27f2c325afb49a
FAC <57494827+fellouhahmedchoukri@users.noreply.github.com>
2025-08-05 20:11:25 +0100

    Update connector.js

---
+++ core/exchange/connector.js
@@ -0,0 +1,15 @@
+import ccxt from 'ccxt';
+
+export const connect = (exchangeId, config) => {
+  const exchangeClass = ccxt[exchangeId];
+  if (!exchangeClass) throw new Error(`Exchange non supporté: ${exchangeId}`);
+  
+  const exchange = new exchangeClass({
+    apiKey: config.apiKey,
+    secret: config.secret,
+    options: { adjustForTimeDifference: true }
+  });
+  
+  exchange.setSandboxMode(false); // Mode production
+  return exchange;
+};
